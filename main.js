2e20e3af603c6a4674c9b739ac19374070eb5944
Arthur Otavio Rodrigues Davidonis <145383664+arthurdavidonisrd@users.noreply.github.com>
2024-03-18 13:55:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ let cart = []
 //Open Modal
 cartBtn.addEventListener("click", function() {
     cartModal.style.display = "flex"
+    updateCartModal()
+    
 })
 
 cartModal.addEventListener("click", function(event){
@@ -30,35 +32,107 @@ closeModalBtn.addEventListener("click", function(){
 //Close Modal
 
 
-
-
+//funcao opcional para fechar modal
 menu.addEventListener("click", function(event){
-    let parentBtn = event.target.closest("add-to-cart-btn")
+    let parentBtn = event.target.closest(".add-to-cart-btn")
 
-    if(parentBtn){
-        const name = parentBtn.getAtributte("data-name")
-        const price = parseFloat(parentBtn.getAtributte("data-price"))
+    if(parentBtn){ 
+        const name = parentBtn.getAttribute("data-name");
+        const price = parseFloat(parentBtn.getAttribute("data-price"))
 
-        AddToCart(name, price)
+        addToCart(name, price)
+    }
+})
+
+//fim da funcao opcional fechar modal
+
+function addToCart(name,price){
+
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.qtd += 1
+    }
+    else{
+        cart.push({
+            name,
+            price,
+            qtd: 1,
+        })
 
     }
 
-    
+    updateCartModal()
+  
+}
+
+//Att cart
+
+function updateCartModal(){
+    cartItensContainer.innerHTML = "";
+    let total = 0
+
+
+    cart.forEach(item =>{
+        const cartItemElement = document.createElement("section")
+
+        cartItemElement.innerHTML = `
+            <section>
+                <div class="mt-6">
+                    <p class="font-medium">nome: ${item.name}</p>
+                    <p class="">Qtd: ${item.qtd}</p>
+                    <p class="font-medium mt-2">R$: ${item.price.toFixed(2)}</p>
+                    <hr>
+
+                </div>
+            </section>
+        `
+
+        total += item.price * item.qtd
+
+        cartItensContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total;
+    cartCounter.innerText = cart.length;
+}
+
+
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressWarning.classList.add("hidden")
+    }
 
 })
 
 
+checkoutBtn.addEventListener("click", function(){
+    if(cart.length === 0 ) return;
+   
+    if(addressInput.value === ""){
+        addressWarning.classList.remove("hidden")
+        addressWarning.style.color = "red"
 
-//function para add ao carrinho
+    }
 
-function AddToCart(name, price){
+    const cartItens = cart.map((item) =>{
+        return(
+           ` ${item.name} Quantidade: (${item.qtd}) Preço: R$ ${item.price} |`
+        )
+    }).join("")
 
-    cart.push({
-        name,
-        price,
-        qtd: 1
-    })
-}
+    const message = encodeURIComponent(cartItens)
+    const phone = "4699331931"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`)
+})
+
+
+
+
 
 
 
